38601aee4270cefca5636ecf95b353c6fdfdd4a1
PaolaFerrete <paola.ferrete@alumni.usp.br>
2024-05-21 13:29:04 +0100

    add ft_putstr in reverse rotate

--- src/reverse_rotate_command.c
+++ src/reverse_rotate_command.c
@@ -6,7 +6,7 @@
 /*   By: paola <paola@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/19 10:24:35 by utente            #+#    #+#             */
-/*   Updated: 2024/05/15 14:18:53 by paola            ###   ########.fr       */
+/*   Updated: 2024/05/21 13:28:45 by paola            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,14 +35,14 @@ void	rra(t_stack_node **a, bool checker)
 {
 	reverse_rotate(a);
 	if (!checker)
-		printf("rra\n");
+		ft_putstr_fd("rra\n", 1);
 }
 
 void	rrb(t_stack_node **b, bool checker)
 {
 	reverse_rotate(b);
 	if (!checker)
-		printf("rrb\n");
+		ft_putstr_fd("rrb\n", 1);
 }
 
 void	rrr(t_stack_node **a, t_stack_node **b, bool checker)
@@ -50,5 +50,5 @@ void	rrr(t_stack_node **a, t_stack_node **b, bool checker)
 	reverse_rotate(a);
 	reverse_rotate(b);
 	if (!checker)
-		printf("rrr\n");
+		ft_putstr_fd("rr\n", 1);
 }
